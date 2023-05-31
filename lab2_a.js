ed1049c2e92fb5b19f09607c222ece1032e3420f
Phạm Đức Anh <ducanh040202003@gmail.com>
2023-05-31 09:58:36 +0700

    Upload Source Test

---
+++ lab2_a.js
@@ -0,0 +1,33 @@
+const express = require ('express');
+const app = express();
+const router = express.Router();
+const now = new Date();
+
+const date = require('date-and-time')
+const value = date.format(now,'YYYY/MM/DD HH:mm:ss');
+
+router.use((req, res, next) => {
+    console.log('Time:', Date.now());
+    next();
+});
+
+router.get('/home', (req, res) => {
+    res.send("Hello Duc Anh");
+});
+
+router.get('/profile', (req, res) => {
+    res.send("Welcom profile Duc Anh");
+});
+
+router.get('/login', (req, res) => {
+    res.send("Welcom login Duc Anh");
+});
+
+router.get('/logout', (req, res) => {
+    res.send("Welcom logout Duc Anh");
+});
+
+app.use('/api', router);
+app.listen(process.env.port || 9000 );
+console.log("Current date and time: " + value);
+console.log('Server running on port' + (process.env.port || 9000));
