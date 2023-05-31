98ce547d4e2dc70493e9aaa0b01fdcced9a5603f
Phạm Đức Anh <ducanh040202003@gmail.com>
2023-05-31 11:18:32 +0700

    Update File Lab_2

--- lab2_a.js
+++ lab2_a.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const app = express();
 const router = express.Router();
+
 const now = new Date();
 
 const date = require('date-and-time')
@@ -31,3 +32,23 @@ app.use('/api', router);
 app.listen(process.env.port || 9000 );
 console.log("Current date and time: " + value);
 console.log('Server running on port' + (process.env.port || 9000));
+
+// ************************************************************************************
+// ************************************************************************************
+
+// router.get('/', function(req, res) {
+//     throw new err();
+//     res.send("Welcom Duc Anh");
+// });
+
+// app.use('/', router);
+
+// app.use(function(err, req, res, next) {
+//    console.log(err.stack); 
+//    res.status(500).send({"Error": err.stack});
+// });
+
+// app.listen(9000);
+
+
+
